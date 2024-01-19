3482b908a80a835cd703cd65b573a9e5ec6d48b2
Elvin <elvinly323@gmail.com>
2024-01-18 21:46:39 -0500

    Fix header URI

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,8 @@
 import logo from '../assets/header-logo.png'
 
 function Header({code, setCode, setSongs, accessToken, setAccessToken}) {
-    const CLIENT_ID = "56f5a961a7f1454ea689c6c5d8fc56ce";
-    const REDIRECT_URI = "http://localhost:5173/";
+    const CLIENT_ID = process.env.CLIENT_ID;
+    const REDIRECT_URI = process.env.REDIRECT_URI;
     const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
     const RESPONSE_TYPE = "code";
     const SCOPE = "playlist-read-private, user-top-read";
